c2e19f90f89ce909453fb3a2bedfac136e8927c5
Djibril Awa Makhtar FALL <djibrilfall92@gmail.com>
2024-09-02 00:56:10 +0000

    projet_python

---
+++ checkpointPython.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 00:06:18 2024
+
+@author: Djibril Awa Makhtar FALL
+"""
+# 1. Ouvrez un nouveau fichier dans votre environnement de développement Python ou IDE.
+
+""" 2. Commencez par taper la ligne « Bienvenue dans Python Pizza Deliveries »
+qui affichera un message de bienvenue lors de l'exécution du code."""
+
+print("Bienvenue dans Python Pizza Deliveries")
+
+""" 3. Ajoutez une ligne qui demande à l'utilisateur la taille de la pizza 
+qu'il souhaite à l'aide de la fonction input() et stocke la saisie de l'utilisateur 
+dans une variable nommée « taille »."""
+
+taille = input("Quelle taille de pizza souhaitez-vous (Petite, Moyenne, Grande) ?")
+
+""" 4. Ajoutez une ligne qui demande à l'utilisateur s'il veut du pepperoni sur 
+sa pizza en utilisant la fonction input() et stocke la saisie de
+ l'utilisateur dans une variable nommée « add_pepperoni »."""
+
+add_pepperoni = input("Voulez-vous du pepperoni sur votre pizza ?")
+
+""" 5. Ajoutez une ligne qui demande à l'utilisateur s'il veut du fromage
+ supplémentaire sur sa pizza en utilisant la fonction input() 
+ et stocke la saisie de l'utilisateur dans une variable nommée « extra_cheese »."""
+ 
+extra_cheese = input("Désiriez-vous du fromage supplémentaire sur votre pizza?")
+
+# 6. Vérifiez votre code pour détecter d'éventuelles erreurs de syntaxe ou fautes.
+
+""" 7. Exécutez le code et testez-le en fournissant des entrées pour la taille,
+ le pepperoni et le fromage supplémentaire."""
+ 
+
+
